75be00561096d38f8a317087789af7de3829c77d
Figby222 <figby222@outlook.com>
2024-03-19 05:52:29 -0700

    Move all functions to prototype

--- src/modules/linked-list.js
+++ src/modules/linked-list.js
@@ -2,29 +2,31 @@ import Node from './node.js';
 const LinkedList = function() {
     this._linkedList = {};
     
-    this.append = function(value) {
-        const node = new Node(value);
-
-        if (!Object.values(this._linkedList)) {
-            this._linkedList = node;
-            return;
-        }
-
-        const formerTail = this.getTail();
-        formerTail.nextNode = node;
-
-
-    }
-
-    this.getTail = function(node) {
-        if (node.nextNode === null) {
-            return node;
-        }
-
-        return this.tail(node.nextNode);
-    }
-
-    this.getList = () => this._linkedList;
+    
 };
+
+LinkedList.prototype.append = function(value) {
+    const node = new Node(value);
+
+    if (!Object.values(this._linkedList)) {
+        this._linkedList = node;
+        return;
+    }
+
+    const formerTail = this.getTail();
+    formerTail.nextNode = node;
+
+
+}
+
+LinkedList.prototype.getTail = function(node) {
+    if (node.nextNode === null) {
+        return node;
+    }
+
+    return this.tail(node.nextNode);
+}
+
+LinkedList.prototype.getList = function() { return this._linkedList };
 
 export default LinkedList;
