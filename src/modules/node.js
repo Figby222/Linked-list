fef4c77ef60291236d0803f7e8ff6789bec84d1c
Figby222 <figby222@outlook.com>
2024-03-19 05:51:11 -0700

    Add getValue() and getNode() on Node prototype

--- src/modules/node.js
+++ src/modules/node.js
@@ -3,5 +3,9 @@ const Node = function(value, nextNode = null) {
     this.nextNode = nextNode;
 }
 
+Node.prototype.getValue = function() { return this.value };
+Node.prototype.getNode = function () { return this.nextNode };
+
+
 
 export default Node;
