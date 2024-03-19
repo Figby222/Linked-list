6a43525109bb01ae1c6fd08b407f995f383bac5b
Figby222 <figby222@outlook.com>
2024-03-19 05:39:33 -0700

    Add LinkedList.- append() & getTail()

--- src/modules/linked-list.js
+++ src/modules/linked-list.js
@@ -1,5 +1,25 @@
 const LinkedList = function() {
+    this._linkedList = {};
+    
+    this.append = function(node) {
+        if (!Object.values(_linkedList)) {
+            _linkedList = node;
+            return;
+        }
 
+        const formerTail = this.getTail();
+        formerTail.nextNode = node;
+
+
+    }
+
+    this.getTail = function(node) {
+        if (node.nextNode === null) {
+            return node;
+        }
+
+        return this.tail(node.nextNode);
+    }
 };
 
 export default LinkedList;
